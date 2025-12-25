2b50315c1b4ff17ab1efc8aa0c3a8a21a371e61e
Umarmajeed711 <umarmajeed711@gmail.com>
2025-12-25 13:21:11 +0500

    Deploy Docusaurus site to GitHub Pages with correct build output

---
+++ assets/js/acecf23e.01d1db10.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkbook=globalThis.webpackChunkbook||[]).push([[1903],{1912(o){o.exports=JSON.parse('{"blogBasePath":"/AI-Driven-Book-Development/blog","blogTitle":"Blog","authorsListPath":"/AI-Driven-Book-Development/blog/authors"}')}}]);
---
+++ assets/js/ffd74f40.2b782529.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkbook=globalThis.webpackChunkbook||[]).push([[2786],{9327(o){o.exports=JSON.parse('{"metadata":{"permalink":"/AI-Driven-Book-Development/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
